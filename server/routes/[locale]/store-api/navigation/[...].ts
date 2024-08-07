6e528d8caecfd9a3297f9108cf41f1a235b53fa9
klarstil <klarstil@googlemail.com>
2024-08-07 09:21:02 +0200

    feat(navigation): reduce response payload

--- server/routes/[locale]/store-api/navigation/[...].ts
+++ server/routes/[locale]/store-api/navigation/[...].ts
@@ -1,12 +1,38 @@
 import { usePrepareRequest } from '~~/utils/usePrepareRequest';
+import type { Schemas } from '#shopware';
 
 export default defineCachedEventHandler(
     async (event) => {
         const { url, requestOptions } = await usePrepareRequest(event);
 
         try {
-            const response = await $fetch(url, requestOptions);
-            return response;
+            const response: Schemas['Category'][] = await $fetch(
+                url,
+                requestOptions,
+            );
+            return response.map((item) => {
+                item.customFields = {};
+                item.media = undefined;
+                item.description = '';
+
+                if (item.translated) {
+                    item.translated.description = '';
+                }
+
+                item.children = item.children.map((child) => {
+                    child.customFields = {};
+                    child.description = '';
+                    child.media = undefined;
+
+                    if (child.translated) {
+                        child.translated.description = '';
+                    }
+
+                    return child;
+                });
+
+                return item;
+            });
         } catch (err) {
             throw createError(err);
         }
