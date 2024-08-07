83cccf57c17cc2c045cbb6b7a8c5763672a5c55e
klarstil <klarstil@googlemail.com>
2024-08-07 13:08:48 +0200

    perf(cache): use force-cache for post requests

--- utils/usePrepareRequest.ts
+++ utils/usePrepareRequest.ts
@@ -26,6 +26,7 @@ export async function usePrepareRequest(event: H3Event) {
     return {
         url: `${targetUrl}${event.path}`,
         requestOptions: {
+            cache: 'force-cache',
             method: event.method,
             body: JSON.stringify(body),
             headers: requestHeaders,
