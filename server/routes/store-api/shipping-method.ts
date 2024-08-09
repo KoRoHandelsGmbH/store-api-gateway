7f17cead8c58886dcac5ce5db8cede6cc421a743
klarstil <klarstil@googlemail.com>
2024-08-09 12:45:44 +0200

    feat(api): respect GET parameter onlyAvaible to disable caching

--- server/routes/store-api/shipping-method.ts
+++ server/routes/store-api/shipping-method.ts
@@ -1,19 +1,19 @@
 import { usePrepareRequest } from '~~/utils/usePrepareRequest';
 
-export default defineCachedEventHandler(
-    async (event) => {
-        const { url, requestOptions } = await usePrepareRequest(event);
+export default defineEventHandler(async (event) => {
+    const query = getQuery(event);
+    const { onlyAvailable } = query;
+    const { url, requestOptions } = await usePrepareRequest(event);
 
-        try {
-            const response = await $fetch(url, requestOptions);
-            return response;
-        } catch (err) {
-            throw createError(err);
-        }
-    },
-    {
-        maxAge: 60 * 1 * 60,
-        swr: true,
-        varies: ['sw-access-key', 'sw-language-id', 'x-env'],
-    },
-);
+    try {
+        const response = await $fetch(url, requestOptions);
+        setResponseHeaders(event, {
+            'Cache-Control': onlyAvailable
+                ? 's-max-age=0, private'
+                : 's-maxage=3600, stale-while-revalidate',
+        });
+        return response;
+    } catch (err) {
+        throw createError(err);
+    }
+});
