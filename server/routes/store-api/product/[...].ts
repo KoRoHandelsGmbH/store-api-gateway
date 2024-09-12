118b27c61cbe8dee5aa6a4765668fbc220a220de
Michael Engel <michael_engel@posteo.de>
2024-09-12 15:06:02 +0200

    feat(useGiftVoucherFilter): set gift voucher unavailable in store api (#1)
    * feat(useGiftVoucherFilter): set gift voucher unavailable in store api
    
    * feat(useGiftVoucherFilter): checks for product number instead of id

--- server/routes/store-api/product/[...].ts
+++ server/routes/store-api/product/[...].ts
@@ -1,11 +1,15 @@
 import { usePrepareRequest } from '~~/utils/usePrepareRequest';
 import { useStoreApiError } from '~~/utils/useStoreApiError';
+import { useGiftVoucherFilter } from '~~/utils/useGiftVoucherFilter';
 
 export default defineEventHandler(async (event) => {
     const { url, requestOptions } = await usePrepareRequest(event);
 
     try {
         const response = await $fetch(url, requestOptions);
+
+        useGiftVoucherFilter(response);
+
         return response;
     } catch (err) {
         throw useStoreApiError(err);
