12b6010f961d13f2eabdf466d0834160de4b03ca
Srihari Thalla <thalla@korodrogerie.de>
2025-02-18 10:08:42 +0100

    perf(load-testing): fix URL

--- tests/load-tests/load-bootup-redis.js
+++ tests/load-tests/load-bootup-redis.js
@@ -4,9 +4,9 @@ import { getOptions, getRecords, performTest } from './functions.js';
 export const options = getOptions;
 
 const baseUrl =
-    'https://store-api-proxy-git-feature-ntrkv-redis-storage-korohandelsgmbh.vercel.app/store-api';
+    'https://store-api-proxy-git-feature-ntrkv-redis-storage-korohandelsgmbh.vercel.app';
 const headers = {
-    Cookie: '_vercel_jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJuU0poeHRqQmZMNWoyeWxKTEMzMUgzR1EiLCJpYXQiOjE3Mzk3OTkxMzksIm93bmVySWQiOiJ0ZWFtX2ZRQVF4MFA3WU1GRlVucVBPVUhVMXVDMSIsImF1ZCI6InN0b3JlLWFwaS1wcm94eS1sb3c5Y3kyeHQta29yb2hhbmRlbHNnbWJoLnZlcmNlbC5hcHAiLCJ1c2VybmFtZSI6ImtsYXJzdGlsIiwic3ViIjoic3NvLXByb3RlY3Rpb24ifQ.ch_bj7D1d7tmdMMKxdo_SKK6UALGCO1GXYkMOm7-HGs',
+    Cookie: '_vercel_jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Vzh6c1NKRTdLbHcxSEpjanJDSkh0UVQiLCJpYXQiOjE3Mzk4NjkwNTAsIm93bmVySWQiOiJ0ZWFtX2ZRQVF4MFA3WU1GRlVucVBPVUhVMXVDMSIsImF1ZCI6InN0b3JlLWFwaS1wcm94eS1naXQtZmVhdHVyZS1udHJrdi1yZWRpcy1zdG9yYWdlLWtvcm9oYW5kZWxzZ21iaC52ZXJjZWwuYXBwIiwidXNlcm5hbWUiOiJ0aGFsbGEta29yb2Ryb2dlcmllIiwic3ViIjoic3NvLXByb3RlY3Rpb24ifQ.wtegc8kxS95p5H62AhYOBPs_m1tKBpKxQdOenaIXeN0',
 };
 
 const records = await getRecords();
